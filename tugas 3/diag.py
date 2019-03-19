3e09ccba83e43080c7eab7a4e63ccafe99e5e0a0
Emerald Makerti <emeraldmakerti@gmail.com>
2019-03-19 17:31:43 +0700

    initial commit

---
+++ tugas 3/diag.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import nltk
+from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
+import numpy as np
+from pathlib import Path
+import math
+
+
+#fetch file
+path = Path(__file__).parents[0]
+inputfile = str(path) + "\\Diagnosa.csv"
+df = pd.read_csv(inputfile, names=["query","diagnosis"], header=0)
+
+
+#preprocessing data
+factory = StemmerFactory()
+stemmer = factory.create_stemmer()
+tokenizer = nltk.RegexpTokenizer(r'\w+')
+
+for index, 
+
+#Bagi file menjadi dua, data Training dan data Testing
+train_data = df[df['diagnosis'].notnull()]
+test_data = df[df['diagnosis'].isnull()]
+train_data.reset_index(inplace=True, drop=True)
+train_data.head()
+test_data.reset_index(inplace=True, drop=True)
+test_data.head()
+
+
+#Jaro similarity
+def jaro_similarity(s1, s2):
+    
