89a63d06a831cf112b49177a4dabfa9cf76495e6
Emerald Makerti <emeraldmakerti@gmail.com>
2019-03-19 19:38:32 +0700

    implemented minkowski distance & added hasil.csv

--- tugas 3/diag.py
+++ tugas 3/diag.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import nltk
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
+from sklearn.feature_extraction.text import CountVectorizer
 import numpy as np
 from pathlib import Path
 import math
+from decimal import Decimal
 
 
 #fetch file
@@ -17,7 +19,20 @@ factory = StemmerFactory()
 stemmer = factory.create_stemmer()
 tokenizer = nltk.RegexpTokenizer(r'\w+')
 
-for index, 
+for index, row in df.iterrows():
+    #stemming
+    stem = stemmer.stem(row[0])
+    #case folding
+    words = stem.lower()
+    df.at[index, "query"] = words
+
+vector = CountVectorizer()
+transform = np.array(vector.fit_transform(df['query']).toarray())
+unique_words = len(transform[0])
+for words in range(unique_words):
+    df.insert(2+words, 'word_{}'.format(words+1), transform[:,words])
+
+df.head()
 
 #Bagi file menjadi dua, data Training dan data Testing
 train_data = df[df['diagnosis'].notnull()]
@@ -27,4 +42,27 @@ train_data.head()
 test_data.reset_index(inplace=True, drop=True)
 test_data.head()
 
+#Minkowski Distance
+def minkowski_distance(x,y,p_value):
+    return nth_root(sum(pow(abs(a-b), p_value) for a, b in zip(x, y)), p_value)
+
+#calculate nth root
+def nth_root(value, n_root):
+    root_value = 1/float(n_root)
+    return round(value ** root_value, 3)
+
+#Calculate distance using Minkowski Distance Algorithm with power set to 3
+test_data.insert(2, 'distance', 0.0)
+for test_index, test_row in test_data.iterrows():
+    distance = []
+    test_vector = np.array(test_row[3:].tolist())
+    for train_index, train_row in train_data.iterrows():
+        train_vector = np.array(train_row[2:].tolist())
+        distance += [minkowski_distance(test_vector, train_vector, 3)]
+    test_data.at[test_index, 'distance'] = np.min(distance)
+    test_data.at[test_index, 'diagnosis'] = train_data.at[np.argmin(distance), 'diagnosis']
+
+print(test_data.iloc[:, :3])
+
+test_data.to_csv('hasil.csv')
 
